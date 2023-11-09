d238d3c18695e9f715cbc444efe9d7a125c46e54
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-09 12:17:32 +0100

    Ejercicio 8 sin terminar2

--- Ejercicio8.cpp
+++ Ejercicio8.cpp
@@ -2,14 +2,31 @@
 #include <string>
 #include <vector>
 
+using namespace std;
 class Estudiante {
 public:
-    Estudiante(const std::string& nombre, const std::string& grado) : nombre(nombre), grado(grado) {}
+    Estudiante(const string& nombre, const string& grado) : nombre(nombre), grado(grado) {}
 
-    const std::string& getGrado() const {
+    const string& getGrado() const {
         return grado;
     }
 
-    const std::string& getNombre() const {
+    const string& getNombre() const {
         return nombre;
     }
+private:
+    string nombre;
+    string grado;
+};
+
+vector<Estudiante> filtrarEstudiantesPorGrado(const vector<Estudiante>& estudiantes, const string& grado) {
+    vector<Estudiante> estudiantesFiltrados;
+
+    for (const Estudiante& estudiante : estudiantes) {
+        if (estudiante.getGrado() == grado) {
+            estudiantesFiltrados.push_back(estudiante);
+        }
+    }
+
+    return estudiantesFiltrados;
+}
