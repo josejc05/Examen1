3ed0c17a6a9072850f027be3f2a4f0a47849b9c1
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-09 11:44:46 +0100

    Ejercicio 2 sin terminar

---
+++ Ejercicio2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Estudiante{
+    private:
+        string nombre;
+        int edad;
+        string grado;
+    public:
+        void mostrar_info(){
+            cout << "Ingrese el nombre del estudiante: ";
+            cin >> nombre;
+            cout << "Ingrese la edad del estudiante: ";
+            cin >> edad;
+            cout << "Ingrese el grado del estudiante: ";
+            cin >> grado;
+            cout<<"Nombre: "<<nombre<<endl;
+            cout<<"Edad: "<<edad<<endl;
+            cout<<"Grado: "<<grado<<endl;
+        }
+};
