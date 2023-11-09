dc96fafb15d0542c80608c71835417fd39d55e24
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-09 12:23:19 +0100

    Ejercicio 9 sin terminar2

--- Ejercicio9.cpp
+++ Ejercicio9.cpp
@@ -14,3 +14,23 @@ public:
 private:
     string materia;
 };
+class Estudiante {
+public:
+    Estudiante(string nombre) : nombre(nombre) {}
+
+    void registrar_materia(const string& materia) {
+        for (const string& materiaRegistrada : materias) {
+            if (materiaRegistrada == materia) {
+                throw MateriaYaRegistradaException(materia);
+            }
+        }
+        materias.push_back(materia);
+    }
+
+    void listar_materias() {
+        cout << nombre << " ha registrado las siguientes materias: ";
+        for (const string& materia : materias) {
+            cout << materia << ", ";
+        }
+        cout << endl;
+    }
