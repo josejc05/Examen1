0dd77bc6c4efc0a9052d4696dbcc814991159fd7
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-09 11:41:08 +0100

    Ejercicio 1

---
+++ Ejercicio1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1, n2;
+    cout << "Ingrese el primer numero: ";
+    cin >> n1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> n2;
+    int suma = n1 + n2;
+    cout << "La suma de los numeros es: " << suma << endl;
+    return 0;
+}
