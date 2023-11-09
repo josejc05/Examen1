37e8c2ff7b0d31d46572c14a9754b8ce651ccce1
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-09 11:51:50 +0100

    Ejercicio 3

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -8,5 +8,12 @@ void intercambiar(int *a, int *b){
 int main(){
     int n1=33, n2=77;
     cout<<"Valores originales: n1="<<n1<<", n2="<<n2<<endl;
+    int* ptr1 = &n1;
+    int* ptr2 = &n2;
+
+    intercambiar(ptr1, ptr2);
+
+    cout << "Valores intercambiados: num1 = " << n1 << ", num2 = " << n2 << endl;
+
     return 0;
 }
