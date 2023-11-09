eafb9819caeb0a00ca7d5f11603bb360d1918d2b
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-09 11:56:35 +0100

    Ejercicio 4 sin terminar2

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int divide(int x, int y) {
-        if (y == 0) {
-            throw "División por cero no está permitida"; // Lanzar una excepción
+
+int divide(int x, int y) {
+    if (y == 0) {
+        throw "División por cero no está permitida"; // Lanzar una excepción
         }
-        return x / y;
+    return x / y;
     }
+
+int main() {
+    int numerador, denominador;
+    cout << "Ingrese el numerador: ";
+    cin >> numerador;
+    cout << "Ingrese el denominador: ";
+    cin >> denominador;
 
     return 0;
 }
